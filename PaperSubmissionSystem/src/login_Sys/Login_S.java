ab84ba19f3e975cf4e94ee512f126728522d1c5d
ChristianMok <33701207+ChristianMok@users.noreply.github.com>
2019-03-03 17:55:25 -0700

    Iteration one basic version

--- PaperSubmissionSystem/src/login_Sys/Login_S.java
+++ PaperSubmissionSystem/src/login_Sys/Login_S.java
@@ -7,20 +7,30 @@ import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-import Signup_Sys.Signup;
+import reviewer.Reviewer;
+import signup_Sys.Signup;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import java.awt.Font;
 import javax.swing.JSeparator;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
+import javax.swing.JRadioButton;
 
 public class Login_S {
 
 	private JFrame frame;
 	private JPasswordField txtPassword;
 	private JTextField txtUserEmail;
+	private String AuthorsInfoPath = "resources/authors.txt";
+	private String ReviewersInfoPath = "resources/reviewers.txt";
+	private String AdministratorInfoPath = "resources/administrator.txt";
 
 	/**
 	 * Launch the application.
@@ -44,6 +54,34 @@ public class Login_S {
 	public Login_S() {
 		initialize();
 	}
+	/**
+	 * Create the boolean checker function for log-in.
+	 */
+	private static boolean checker(String useremail, String userpassword, String accountpath) {
+		String line = null;
+		boolean Vaild = false;
+		try {
+			// FileReader reads text files in the default encoding.
+			FileReader fileReader = new FileReader(accountpath);
+
+			// Always wrap FileReader in BufferedReader.
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+			while ((line = bufferedReader.readLine()) != null) {
+				String[] token = line.split(";");
+				if (token[0].equals(useremail) && token[1].equals(userpassword)) {
+					Vaild = true;
+					return Vaild;
+				}
+			}
+			bufferedReader.close();
+		} catch (FileNotFoundException ex) {
+			System.out.println("Unable to open file '" + accountpath + "'");
+		} catch (IOException ex) {
+			System.out.println("Error reading file '" + accountpath + "'");
+		}
+		return Vaild;
+	}
 
 	/**
 	 * Initialize the contents of the frame.
@@ -51,71 +89,113 @@ public class Login_S {
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.PINK);
-		frame.setBounds(200, 200, 500, 300);
+		frame.setBounds(200, 200, 500, 342);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
-		JLabel lbUserEmail = new JLabel("UserEmail");
+
+		JLabel lbUserEmail = new JLabel("UserEmail:");
+		lbUserEmail.setBounds(71, 62, 69, 17);
 		lbUserEmail.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lbUserEmail.setBounds(71, 64, 66, 14);
 		frame.getContentPane().add(lbUserEmail);
-		
-		JLabel lbPassword = new JLabel("Password");
+
+		JLabel lbPassword = new JLabel("Password:");
+		lbPassword.setBounds(71, 121, 89, 23);
 		lbPassword.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lbPassword.setBounds(71, 119, 89, 23);
 		frame.getContentPane().add(lbPassword);
-		
+
 		txtPassword = new JPasswordField();
-		txtPassword.setBounds(212, 121, 86, 20);
+		txtPassword.setBounds(212, 121, 130, 20);
 		frame.getContentPane().add(txtPassword);
-		
+
 		txtUserEmail = new JTextField();
-		txtUserEmail.setBounds(212, 62, 86, 20);
+		txtUserEmail.setBounds(212, 62, 130, 20);
 		frame.getContentPane().add(txtUserEmail);
 		txtUserEmail.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("Login System");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel.setBounds(190, 14, 176, 25);
+
+		JLabel lblNewLabel = new JLabel("Paper Submission System");
+		lblNewLabel.setBounds(134, 12, 227, 18);
+		lblNewLabel.setFont(new Font("Montserrat SemiBold", Font.PLAIN, 17));
 		frame.getContentPane().add(lblNewLabel);
-		
+
 		JButton btnLogin = new JButton("Login");
-		btnLogin.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				String password = txtPassword.getText();
-				String username = txtUserEmail.getText();
-				
-			}
-		});
-		btnLogin.setBounds(71, 211, 89, 23);
+		btnLogin.setBounds(71, 237, 89, 23);
 		frame.getContentPane().add(btnLogin);
-		
+
+		JRadioButton rdbtnadministrator = new JRadioButton("Administrator");
+		rdbtnadministrator.setBounds(59, 161, 124, 26);
+		rdbtnadministrator.setBackground(Color.PINK);
+		rdbtnadministrator.setFont(new Font("Dialog", Font.PLAIN, 15));
+		frame.getContentPane().add(rdbtnadministrator);
+
+		JRadioButton rdbtnReviewer = new JRadioButton("Reviewer");
+		rdbtnReviewer.setBounds(210, 161, 92, 26);
+		rdbtnReviewer.setFont(new Font("Dialog", Font.PLAIN, 15));
+		rdbtnReviewer.setBackground(Color.PINK);
+		frame.getContentPane().add(rdbtnReviewer);
+
+		JRadioButton rdbtnAuthor = new JRadioButton("Author");
+		rdbtnAuthor.setBounds(357, 161, 74, 26);
+		rdbtnAuthor.setFont(new Font("Dialog", Font.PLAIN, 15));
+		rdbtnAuthor.setBackground(Color.PINK);
+		frame.getContentPane().add(rdbtnAuthor);
+
+		// Name the RadioButton
+		rdbtnadministrator.setActionCommand("administrator");
+		rdbtnReviewer.setActionCommand("reviewer");
+		rdbtnAuthor.setActionCommand("authors");
+
+		// Group the radio buttons.
+		ButtonGroup group = new ButtonGroup();
+		group.add(rdbtnadministrator);
+		group.add(rdbtnReviewer);
+		group.add(rdbtnAuthor);
 		JButton btnReset = new JButton("Reset");
+		btnReset.setBounds(201, 237, 89, 23);
 		btnReset.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				txtUserEmail.setText(null);
 				txtPassword.setText(null);
+				group.clearSelection();
 			}
 		});
-		btnReset.setBounds(212, 211, 89, 23);
 		frame.getContentPane().add(btnReset);
-		
-		JSeparator separator = new JSeparator();
-		separator.setBounds(21, 192, 426, 2);
-		frame.getContentPane().add(separator);
-		
+
 		JSeparator separator_1 = new JSeparator();
 		separator_1.setBounds(21, 37, 432, 2);
 		frame.getContentPane().add(separator_1);
-		
+
 		JButton btnSignup = new JButton("Sign up");
+		btnSignup.setBounds(328, 237, 89, 23);
 		btnSignup.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Signup info = new Signup();
 				Signup.main(null);
 			}
 		});
-		btnSignup.setBounds(341, 211, 89, 23);
 		frame.getContentPane().add(btnSignup);
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String password = String.copyValueOf(txtPassword.getPassword());
+				String username = txtUserEmail.getText();
+				String selection = group.getSelection().getActionCommand();
+				if (selection == "reviewer") {
+					if (checker(username, password, ReviewersInfoPath) == true) {
+						Reviewer.main(null);
+						
+					}
+				}
+				if (selection == "authors") {
+					if (checker(username, password, AuthorsInfoPath) == true) {
+						System.out.println("456");
+					}
+				}
+				if (selection == "administrator") {
+					if (checker(username, password, AdministratorInfoPath) == true) {
+						System.out.println("789");
+					}
+				}
+			}
+		});
+
 	}
 }
