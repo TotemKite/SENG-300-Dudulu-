d306459393acf36674442ffd18552a450d0cf01e
Sophia Zhu <sophiazhu@sophiedeMacBook-Pro.local>
2019-03-29 14:54:43 -0600

    update junit

--- PaperSubmissionSystem/src/reviewer/Reviewer.java
+++ PaperSubmissionSystem/src/reviewer/Reviewer.java
@@ -64,6 +64,10 @@ public class Reviewer {
 		initialize();
 
 	}
+	
+	public Reviewer(String test) {
+		
+	}
 
 	/**
 	 * Initialize the contents of the frame.
@@ -276,5 +280,21 @@ public class Reviewer {
 			);
 		wholePanel.setLayout(gl_panel);
 		return wholePanel;
+	}
+	
+	
+	public String getFileName(int index) {
+		File folder = new File("test_folder");
+		File[] listOfFiles = folder.listFiles();
+
+		for (int i = 0; i < listOfFiles.length; i++) {
+		  if (listOfFiles[i].isFile()) {
+		    System.out.println("File " + listOfFiles[i].getName());
+		  } else if (listOfFiles[i].isDirectory()) {
+//		    System.out.println("Directory " + listOfFiles[i].getName());
+		  }
+		}
+		String fileName = listOfFiles[index].getName();
+		return fileName;
 	}
 }
---
+++ PaperSubmissionSystem/src/reviewer/TestReviewer.java
@@ -0,0 +1,20 @@
+package reviewer;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class TestReviewer {
+	String testFlag = null;
+	Reviewer re = new Reviewer(testFlag);
+	@Test
+	public void testAIBoundCheck_InsideNotHit() throws Exception{
+		
+//		System.out.println(world[2][2]);
+		assertEquals("3.txt", re.getFileName(0));
+		assertEquals("2.txt", re.getFileName(1));
+		assertEquals("1.txt", re.getFileName(2));
+	}
+	
+	@Test
+	public void testWhole_Reviewer(){
+		Reviewer.main(null);
+	}
+}
