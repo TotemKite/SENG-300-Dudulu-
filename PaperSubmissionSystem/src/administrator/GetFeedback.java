60761fc7477fccbded07619eb81fb1e4d9eb2721
Sophia Zhu <sophiazhu@sophiedeMacBook-Pro.local>
2019-04-09 20:58:18 -0600

    update

--- PaperSubmissionSystem/src/administrator/GetFeedback.java
+++ PaperSubmissionSystem/src/administrator/GetFeedback.java
@@ -2,8 +2,16 @@ package administrator;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.IOException;
 
 import javax.swing.JFrame;
 import javax.swing.JTextField;
@@ -13,6 +21,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
 
 public class GetFeedback {
 
@@ -55,60 +64,94 @@ public class GetFeedback {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 550, 500);
 		frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		
+
 		lblMajorFeedback = new JLabel("Major Feedback");
 		lblMajorFeedback.setBounds(33, 21, 147, 26);
-		
+
 		lblMinorFeedback = new JLabel("Minor Feedback");
 		lblMinorFeedback.setBounds(337, 21, 147, 26);
+
+		JButton acceptButton = new JButton("ACCEPT");
+		acceptButton.setBounds(200, 250, 100, 30);
+//		acceptButton.setLocation(150, 150);
+		frame.add(acceptButton);
+		acceptButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				FileOutputStream outputStream;
+				try {
+					String newFilePath = "submissions/approved/" + username + ".txt";
+					String line = null;
+					// FileReader reads text files in the default encoding.
+					FileReader fileReader = new FileReader("submissions/reviewed/" + username + "_reviewed.txt");
+					outputStream = new FileOutputStream(newFilePath);
+					// Always wrap FileReader in BufferedReader.
+					BufferedReader bufferedReader = new BufferedReader(fileReader);
+					DataOutputStream dataOutStream = new DataOutputStream(new BufferedOutputStream(outputStream));
+					while ((line = bufferedReader.readLine()) != null) {
+						dataOutStream.writeUTF(line);
+					}
+					bufferedReader.close();
+					dataOutStream.close();
+					File deleteFile = new File("submissions/reviewed/" + username + "_reviewed.txt");
+					deleteFile.delete();
+					frame.dispose();
+				} catch (FileNotFoundException ex) {
+//					System.out.println("Unable to open file '" + filePath + "'");
+					ex.printStackTrace();
+				} catch (IOException ex) {
+//					System.out.println("Error reading file '" + filePath + "'");
+				}
+			}
+		});
 	}
-	
+
 	private void readMajorFeedBack() {
 		JTextArea textArea = new JTextArea();
 		JPanel panel = new JPanel();
 		panel.setBounds(21, 50, 153, 340);
 		JScrollPane scroll = new JScrollPane(textArea);
 		scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        panel.setLayout(new BorderLayout());
-        textArea.setEditable(false);
+		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		panel.setLayout(new BorderLayout());
+		textArea.setEditable(false);
 		panel.add(scroll);
 		frame.getContentPane().setLayout(null);
 		frame.getContentPane().add(lblMajorFeedback);
 		frame.getContentPane().add(lblMinorFeedback);
 		frame.getContentPane().add(panel);
 		try {
-			FileReader reader = new FileReader( "feedback/" + username + "_Major.txt");
+			FileReader reader = new FileReader("feedback/" + username + "_Major.txt");
 			BufferedReader br = new BufferedReader(reader);
 			textArea.read(br, null);
 			br.close();
 			textArea.requestFocus();
-		} catch(Exception e) {
+		} catch (Exception e) {
 		}
 	}
+
 	private void readMinorFeedBack() {
-		
+
 		JTextArea textArea_1 = new JTextArea();
 		JPanel panel1 = new JPanel();
 		panel1.setBounds(331, 50, 153, 340);
 		JScrollPane scroll1 = new JScrollPane(textArea_1);
 		scroll1.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scroll1.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        panel1.setLayout(new BorderLayout());
-        textArea_1.setEditable(false);
+		scroll1.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		panel1.setLayout(new BorderLayout());
+		textArea_1.setEditable(false);
 		panel1.add(scroll1);
 		frame.getContentPane().setLayout(null);
 		frame.getContentPane().add(lblMajorFeedback);
 		frame.getContentPane().add(lblMinorFeedback);
 		frame.getContentPane().add(panel1);
 		try {
-			FileReader reader = new FileReader( "feedback/" + username + "_Minor.txt");
+			FileReader reader = new FileReader("feedback/" + username + "_Minor.txt");
 			BufferedReader br = new BufferedReader(reader);
 			textArea_1.read(br, null);
 			br.close();
 			textArea_1.requestFocus();
-		} catch(Exception e) {
+		} catch (Exception e) {
 		}
-		
+
 	}
 }
